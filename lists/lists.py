7f07cc1bcc285f35a99f0bf937246eba7455943a
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2021-10-17 10:10:46 +0600

    Update lists.py

--- lists/lists.py
+++ lists/lists.py
@@ -1,18 +1,27 @@
 def size_of_list(list):
-    # return size of list
+    return(len(list))
 
 def add_elem_to_list(list, elem):
-    # add elem to list and return the list
+    list.append(elem)
+    return list
 
 def delete_elem_from_list(list, index = -1):
     # delete element from list, such that its index is index
-    # if index is invalid, return empty list
+    if len(list) < index:
+        return []
+    return list.pop(index)
 
 def count_elements_in_list(list, x):
     # count elements in the list that are equal to x and return the count
+    occurences = list.count(x)
+    return occurences
 
 def sort_list(list):
     # return sorted list
+    sorted = list.sort()
+    return sorted
 
 def reverse(list):
     # return reversed list
+    reversed = list.reverse()
+    return reversed
