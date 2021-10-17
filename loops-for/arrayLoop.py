e925910ceb475f16d975ab9f867d225e7e8082b1
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2021-10-17 11:01:37 +0600

    Update arrayLoop.py

--- loops-for/arrayLoop.py
+++ loops-for/arrayLoop.py
@@ -2,5 +2,5 @@ def insert_squares(arr, num):
     # add square of numbers from 1 to num to the list named arr and return list
     for i in range(1, num + 1):
         arr.append(i * i)
-    return ar
+    return arr
         
