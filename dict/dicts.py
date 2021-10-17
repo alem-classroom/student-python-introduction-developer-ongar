7a605a4efa6820bb10feb3a20357b0ea585837c6
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2021-10-17 10:15:30 +0600

    Update dicts.py

--- dict/dicts.py
+++ dict/dicts.py
@@ -1,14 +1,16 @@
 def clear_dict(dict):
-    # delete everything in dict and return it
+    return  dict.clear()
 
 def get_dict_items(dict):
-    # return keys and values of dict
+    return dict.items()
 
 def get_dict_keys(dict):
-    # return keys of dict
+    return dict.keys()
 
 def get_dict_value_by_key(dict, key):
-    # return values of dict that is stored in key
+    if(dict.get(key)):
+        return dict[key]
 
 def delete_dict_element_by_key(dict, key):
-    # delete and element from dict, such that its key is the argument key
+    dict.pop(key)
+    return dict
